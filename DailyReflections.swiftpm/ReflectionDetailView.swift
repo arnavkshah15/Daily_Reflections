11aa51c405a0c8ba0524b7b24c40c2dd0c6fd835
arnavkshah15 <100327793+arnavkshah15@users.noreply.github.com>
2024-02-15 08:54:58 +0530

    added strengths

--- DailyReflections.swiftpm/ReflectionDetailView.swift
+++ DailyReflections.swiftpm/ReflectionDetailView.swift
@@ -7,10 +7,33 @@ struct ReflectionDetailView: View {
     let questions = ["What was one challenging situation or problem you encountered today?",
                      "Could you overcome the challenge? If yes, how did you manage it? If no, what do you think might be the reason?",
                      "What was the most valuable advice or opinion someone shared with you today?"]
-    
+    let strengths = [
+           "Creativity",
+           "Problem-solving",
+           "Teamwork",
+           "Leadership",
+           "Communication",
+           "Adaptability",
+           "Time Management",
+           // Add more strengths as needed
+       ]
+    func selectRandomThree<T>(_ array: [T]) -> [T] {
+            var shuffledArray = array.shuffled()
+            return Array(shuffledArray.prefix(3))
+        }
     var body: some View {
         VStack(alignment: .leading) {
-            
+            Text("Strengths")
+                                .font(.title3)
+                                .bold()
+                                .padding()
+                            
+                            ForEach(selectRandomThree(strengths), id: \.self) { strength in
+                                Text(strength)
+                                    .padding(.horizontal)
+                                    .foregroundColor(.blue)
+                                    .font(.headline)
+                            }
             
             
             ForEach(0..<reflection.answers.count, id: \.self) { index in
